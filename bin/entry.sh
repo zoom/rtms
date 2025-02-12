9a907c291389a26c39adb154a16c3171c4a1db28
Max Mansfield <max.mansfield@zoom.us>
2025-02-12 12:52:49 -0700

    cleaned code

--- bin/entry.sh
+++ bin/entry.sh
@@ -3,17 +3,12 @@
 set -e
 
 
-lang="${RTMS_SDK_LANG}" # Assign the environment variable to a regular variable for easier use
+lang="${LANG:-js}"
 
-if [ -z "$lang" ]; then
-  echo "RTMS_SDK_LANG is blank. Defaulting to 'js'..."
-  lang=js
-fi
 
 if [ "$lang" == "js" ] && [ ! -d "node_modules" ]; then 
     npm install;
 fi
-
 
 
 cmd="npm run build-$lang" 
