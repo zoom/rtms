cc6fc7a0636e712196907ee4d7eb252e41d80106
Max Mansfield <max.mansfield@zoom.us>
2025-03-26 01:17:34 -0600

    fixed untarring

--- scripts/common/frameworks.js
+++ scripts/common/frameworks.js
@@ -1,21 +1,29 @@
 import { promises as fs } from 'fs';
 import { join, dirname } from 'path';
 import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+
 
 import { log, getBuildMode } from './utils.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const buildMode = getBuildMode();
 const buildDir= buildMode === 'debug' ? 'Debug' : 'Release';
 
 const PREFIX = 'Frameworks';
 
 async function extractFrameworks() {
-    log(PREFIX, 'Extracting Framworks...');
     const require = createRequire(import.meta.url);
     const tar = require('tar');
     
-    const dir = join(dirname(import.meta.url), `build/${buildDir}`);
+    const dir = join(__dirname, `../../build/${buildDir}/`);
+    log(PREFIX, `Extracting Framworks... ${dir}`);
     const frameworksDir = dir
     const outputDir = dir
+
+    log(PREFIX, `Extracting Framworks... ${frameworksDir}`);
 
     try {
         // Create output directory if it doesn't exist
@@ -23,6 +31,7 @@ async function extractFrameworks() {
         
         // Get all tar files
         const files = await fs.readdir(frameworksDir);
+        console.log("Files: ", files);
         const tarFiles = files.filter(file => file.endsWith('.tar.gz'));
         
         console.log(`Found ${tarFiles.length} framework archives to extract`);
