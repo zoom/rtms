f2c017a49e00af480de93e0c4982a70da8a741bf
Max Mansfield <max.mansfield@zoom.us>
2025-04-03 19:38:54 -0600

    improved clean scripts for rollup

--- scripts/common/clean.js
+++ scripts/common/clean.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { log, getProjectRoot, removeDir, success } from './utils.js';
+import { log, getProjectRoot, executeScript, removeDir, success } from './utils.js';
 import { join } from 'path';
 
 const PREFIX = "Clean";
@@ -11,14 +11,27 @@ function cleanProject() {
   // Clean build artifacts
   removeDir(join(rootDir, 'dist'), PREFIX);
   removeDir(join(rootDir, 'build'), PREFIX);
-  removeDir(join(rootDir, 'prebuilds'), PREFIX);
   removeDir(join(rootDir, 'docs'), PREFIX);
-  
-  // Clean temp files
-  removeDir(join(rootDir, 'node_modules/.cache'), PREFIX);
+  removeDir(join(rootDir, 'node_modules'), PREFIX);
+
   
   success(PREFIX, 'Clean completed successfully');
 }
 
-// Run the clean operation
-cleanProject();
+function cleanAll() {
+  const rootDir = getProjectRoot();
+  
+  cleanProject();
+
+  // Clean prebuild artifacts
+  removeDir(join(rootDir, 'prebuilds'), PREFIX);
+  
+  success(PREFIX, 'Prebuild clean completed successfully');
+}
+
+
+// Run using our common execution wrapper
+executeScript(PREFIX, {
+  all: cleanAll,
+  project: cleanProject,
+});
